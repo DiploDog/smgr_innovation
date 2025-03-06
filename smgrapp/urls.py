79355cf88499babe67b4ff282368f92d497a281b
DiploDog <mechenu@icloud.com>
2025-03-06 17:50:59 +0300

    improved routing

--- smgrapp/urls.py
+++ smgrapp/urls.py
@@ -5,9 +5,9 @@ from is_innovative import views
 
 urlpatterns = [
     path("", views.index, {"header": "Проверка инновационности вагона"}),
-    path("postuser/", views.postuser),
-    #path("values/", views.calculation),
+    path("car-type/", views.select_car_type),
     path("values/", views.calculation),
+    path("postuser/", views.postuser),
     path("about/",
          TemplateView.as_view(template_name="about.html"),
          {"header": "О приложении"},
